ed0b911ef93b497fda86e28c2f85a6a9df16ab6c
White-Chen <q953387601@163.com>
2016-04-06 18:01:58 +0800

    demo function

--- demo.m
+++ demo.m
@@ -1,7 +1,11 @@
 function demo()
+
 clear;
 clc;
-global k l M step window itrCounter dynamic;
+
+%please global these variables
+global k l M step window itrCounter dynamic
+
 dynamic = 1;
 step = 10;
 window = 200;
@@ -9,8 +13,7 @@ itrCounter = 1;
 M = 2;
 k = 20;
 l = 4;
+
 mop = testmop('ud1',11);
-subproblems = moead( mop, 'popsize', 100, 'niche', 20, 'iteration', 2000, 'method', 'te');
-%pareto = moead( mop, 'popsize', 100, 'niche', 20, 'iteration', 200, 'method', 'ws');
 
 end
