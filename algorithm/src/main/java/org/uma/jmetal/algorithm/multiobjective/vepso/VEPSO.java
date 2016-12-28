5902928d8e9199024d3024325b5ae02a7b7dea5d
White-Chen <q953387601@163.com>
2016-12-28 21:30:00 +0800

    Init VEPSO algorithm

---
+++ algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/vepso/VEPSO.java
@@ -0,0 +1,37 @@
+package org.uma.jmetal.algorithm.multiobjective.vepso;
+
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.solution.DoubleSolution;
+
+import java.util.List;
+
+/**
+ * \* Created with Chen Zhe on 12/28/2016.
+ * \* Description:
+ * \* @author ChenZhe
+ * \* @author q953387601@163.com
+ * \* @version 1.0.0
+ * \
+ */
+public class VEPSO implements Algorithm<List<DoubleSolution>> {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public List<DoubleSolution> getResult() {
+        return null;
+    }
+}
