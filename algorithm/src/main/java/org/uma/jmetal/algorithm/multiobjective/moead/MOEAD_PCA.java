897c2b5addf208b2ac8bcd9cebbbeee0d92d6109
zhoulifa <zhoulifa@126.com>
2017-03-22 12:26:21 +0800

    git commit -m "MOEAD_PCA add"

---
+++ algorithm/src/main/java/org/uma/jmetal/algorithm/multiobjective/moead/MOEAD_PCA.java
@@ -0,0 +1,248 @@
+package org.uma.jmetal.algorithm.multiobjective.moead;
+
+import org.uma.jmetal.algorithm.multiobjective.moead.util.MOEADUtils;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.impl.crossover.DifferentialEvolutionCrossover;
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.pca.PrincipalComponentAnalysis;
+import org.uma.jmetal.util.solutionattribute.impl.ExtendDominationRanking;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Zhou-LF on 2017/3/20.
+ */
+public class MOEAD_PCA extends AbstractMOEAD<DoubleSolution> {
+
+    private DifferentialEvolutionCrossover differentialEvolutionCrossover;
+    private PrincipalComponentAnalysis pca;
+
+    private double[] weightOfPopulation;
+    int numplieMax;
+    int numplieMin;
+    int[] hierarchy;
+    int numberOfVariables;
+    double[][] weightOfNeighbor;
+
+    public MOEAD_PCA(Problem<DoubleSolution> problem,
+                 int populationSize,
+                 int resultPopulationSize,
+                 int maxEvaluations,
+                 MutationOperator<DoubleSolution> mutation,
+                 CrossoverOperator<DoubleSolution> crossover,
+                 FunctionType functionType,
+                 String dataDirectory,
+                 double neighborhoodSelectionProbability,
+                 int maximumNumberOfReplacedSolutions,
+                 int neighborSize,
+                 String inProcessDataPath) {
+        super(problem, populationSize, resultPopulationSize, maxEvaluations, crossover, mutation, functionType,
+                dataDirectory, neighborhoodSelectionProbability, maximumNumberOfReplacedSolutions,
+                neighborSize, inProcessDataPath);
+
+        differentialEvolutionCrossover = (DifferentialEvolutionCrossover) crossoverOperator;
+        pca = new PrincipalComponentAnalysis();
+    }
+
+    @Override
+    public void run() {
+        initializePopulation();
+        initializeUniformWeight();
+        initializeNeighborhood();
+        initializeIdealPoint();
+
+        numberOfVariables = problem.getNumberOfVariables();
+        weightOfNeighbor = new double[populationSize][numberOfVariables];
+
+        evaluations = populationSize;
+        do {
+            int[] permutation = new int[populationSize];
+            MOEADUtils.randomPermutation(permutation, populationSize);
+
+            ifPCA(evaluations/populationSize);
+            hierarchy = hierarchySolution();
+            setWeightOfPopulation();
+
+            for (int i = 0; i < populationSize; i++) {
+                int subProblemId = permutation[i];
+
+                NeighborType neighborType = chooseNeighborType();
+                List<DoubleSolution> parents = parentSelection(subProblemId, neighborType);
+
+                differentialEvolutionCrossover.setCurrentSolution(population.get(subProblemId));
+                //设置权重
+                if (neighborType == NeighborType.NEIGHBOR){
+                    differentialEvolutionCrossover.setWeight(weightOfNeighbor[numplieMin]);
+                }else {
+                    differentialEvolutionCrossover.setWeight(weightOfPopulation);
+                }
+                List<DoubleSolution> children = differentialEvolutionCrossover.execute(parents);
+
+                DoubleSolution child = children.get(0);
+                mutationOperator.execute(child);
+                problem.evaluate(child);
+
+                evaluations++;
+
+                updateIdealPoint(child);
+                updateNeighborhood(child, subProblemId, neighborType);
+            }
+            saveDataInProcess();
+        } while (evaluations < maxEvaluations);
+
+    }
+
+    public int[] hierarchySolution(){
+        ExtendDominationRanking<DoubleSolution> extendDominationRanking = new ExtendDominationRanking();
+        extendDominationRanking.computeExtendRanking(population, extendFront);
+        int[] hierarchyIndex = extendDominationRanking.getHierarchyIndex();
+        return hierarchyIndex;
+    }
+
+    public void ifPCA(int itrCounter){
+        if ((itrCounter % 18) == 1){
+            for (int i = 0; i < populationSize; i++){
+                double[] weight1 = setWeightOfNeighbor(i, numberOfVariables);
+                System.arraycopy(weight1, 0, weightOfNeighbor[i], 0, numberOfVariables);
+            }
+        }
+    }
+
+    //对邻域的个体进行主成分分析
+    public double[] setWeightOfNeighbor(int subProblemId, int numberOfVariables){
+        double[] weight;
+        double[][] pcaData = new double[neighborSize][numberOfVariables];
+
+        // System.out.println("*********************");
+        for (int j = 0; j < neighborSize; j++){
+            for (int k = 0; k < numberOfVariables; k++){
+                pcaData[j][k] = population.get(neighborhood[subProblemId][j]).getVariableValue(k);
+                //      System.out.print(pcaData[j][k]+" ");
+            }
+            //  System.out.println();
+        }
+        weight = pca.weightFromPCA(pcaData, neighborSize, numberOfVariables);
+        return weight;
+    }
+
+    //对所有个体进行主成分分析
+    public double[] setWeightOfPopulation(){
+        double[][] populationData = new double[populationSize][numberOfVariables];
+        for (int j = 0; j < populationSize; j++){
+            for (int k = 0; k < numberOfVariables; k++){
+                populationData[j][k] = population.get(j).getVariableValue(k);
+            }
+        }
+        weightOfPopulation = pca.weightFromPCA(populationData, populationSize, numberOfVariables);
+        return weightOfPopulation;
+    }
+
+    protected List<DoubleSolution> parentSelection(int subProblemId, NeighborType neighborType) {
+        List<Integer> matingPool = matingSelection(subProblemId, 2, neighborType);
+
+        List<DoubleSolution> parents = new ArrayList<>(3);
+        parents.add(population.get(matingPool.get(0)));
+        parents.add(population.get(matingPool.get(1)));
+        parents.add(population.get(subProblemId));
+        return parents;
+    }
+
+    protected List<Integer> matingSelection(int subproblemId, int numberOfSolutionsToSelect, NeighborType neighbourType) {
+        Random random = new Random();
+        int neighbourSize;
+        int numplieOfMaxIndex;
+        int numplieOfMinIndex;
+        List<Integer> numplieOfMaxList = new ArrayList<>();
+        List<Integer> numplieOfMinList = new ArrayList<>();
+
+        List<Integer> listOfSolutions = new ArrayList<>(numberOfSolutionsToSelect);
+        neighbourSize = neighborhood[subproblemId].length;
+
+        if(neighbourType == NeighborType.NEIGHBOR){
+            numplieOfMaxIndex = neighborhood[subproblemId][0];
+            numplieOfMinIndex = neighborhood[subproblemId][0];
+
+            //寻找最大最小值
+            for (int i = 1; i < neighbourSize; i++){
+                if (hierarchy[numplieOfMaxIndex] < hierarchy[neighborhood[subproblemId][i]]){
+                    numplieOfMaxIndex = neighborhood[subproblemId][i];
+                }
+                if (hierarchy[numplieOfMinIndex] > hierarchy[neighborhood[subproblemId][i]]){
+                    numplieOfMinIndex = neighborhood[subproblemId][i];
+                }
+            }
+            //找出相同的最大最小值
+            for (int i = 0; i < neighbourSize; i++){
+                if (hierarchy[numplieOfMaxIndex] == hierarchy[neighborhood[subproblemId][i]]){
+                    numplieOfMaxList.add(neighborhood[subproblemId][i]);
+                }
+                if (hierarchy[numplieOfMinIndex] == hierarchy[neighborhood[subproblemId][i]]){
+                    numplieOfMinList.add(neighborhood[subproblemId][i]);
+                }
+            }
+        }else {
+            numplieOfMaxIndex = 0;
+            numplieOfMinIndex = 0;
+
+            //寻找最大最小值
+            for (int i = 1; i < populationSize; i++){
+                if (hierarchy[numplieOfMaxIndex] < hierarchy[i]){
+                    numplieOfMaxIndex = i;
+                }
+                if (hierarchy[numplieOfMinIndex] > hierarchy[i]){
+                    numplieOfMinIndex = i;
+                }
+            }
+            //找出相同的最大最小值
+            for (int i = 0; i < populationSize; i++){
+                if (hierarchy[numplieOfMaxIndex] == hierarchy[i]){
+                    numplieOfMaxList.add(i);
+                }
+                if (hierarchy[numplieOfMinIndex] == hierarchy[i]){
+                    numplieOfMinList.add(i);
+                }
+            }
+        }
+
+        //随机选择最大最小值
+        numplieOfMaxIndex = numplieOfMaxList.get(random.nextInt(numplieOfMaxList.size()));
+        numplieOfMinIndex = numplieOfMinList.get(random.nextInt(numplieOfMinList.size()));
+        while (numplieOfMinIndex == numplieOfMaxIndex){
+            numplieOfMinIndex = numplieOfMinList.get(random.nextInt(numplieOfMinList.size()));
+        }
+
+        listOfSolutions.add(numplieOfMaxIndex);
+        listOfSolutions.add(numplieOfMinIndex);
+
+        numplieMax = numplieOfMaxIndex;
+        numplieMin = numplieOfMinIndex;
+//        System.out.println(numplieMin+" "+numplieMax);
+        return listOfSolutions;
+    }
+
+    protected void initializePopulation() {
+        for (int i = 0; i < populationSize; i++) {
+            DoubleSolution newSolution1 = problem.createSolution();
+            DoubleSolution newSolution2 = problem.createSolution();
+
+            problem.evaluate(newSolution1);
+            problem.evaluate(newSolution2);
+            population.add(newSolution1);
+            extendFront.add(newSolution2);
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "MOEAD_PCA";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Multi-Objective Evolutionary Algorithm based on Decomposition and PCA";
+    }
+}
